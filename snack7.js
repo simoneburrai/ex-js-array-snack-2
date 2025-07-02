423f0d9ddba719e7936aaba417c87b2bfd3e7646
simoneburrai <burrai.s98@gmail.com>
2025-07-02 12:53:15 +0200

    err: problem solving snack7

---
+++ snack7.js
@@ -0,0 +1,14 @@
+const books = require("./data")
+
+
+// - ✅ Usa `.reduce()` per creare `tagCounts`,
+//  un oggetto che conta quante volte ogni tag è usato tra tutti i libri.
+
+console.log(books);
+
+const tagCounts = books.reduce((acc, val)=>{
+    
+}, {})
+
+
+//PROBLEMI NELLA RISOLUZIONE, DA RIVEDERE
